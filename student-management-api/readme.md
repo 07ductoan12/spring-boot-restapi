4f107c1cef9df26e86f8d926957aed6e34f5d07a
07ductoan12 <07ductoan12@gmail.com>
2024-08-13 16:37:31 +0700

    third restapi project: students mamagement

---
+++ student-management-api/readme.md
@@ -0,0 +1,6 @@
+# Summary
+
+- Implement APIs for CRUD operations with <span style="color:red">@GetMapping</span>,<span style="color:red">@PostMapping</span>, <span style="color:red">@PutMapping</span>, <span style="color:red">@GetMapping</span> and <span style="color:red">@DeleteMapping</span>
+- Use <span style="color:red">@RequestMapping("/uri")</span> at class level that acts as the base URI
+- return different HTTP status code conditionally: 200 OK, 201, Created, 204 No Content, 404 Not Found via <span style="color:red">ResponseEntity</span> and <span style="color:red">HttpStatus</span>
+- No datbase operations
---
+++ student-management-api/src/main/java/com/toan/student_management_api/Student.java
@@ -0,0 +1,61 @@
+package com.toan.student_management_api;
+
+import java.util.Objects;
+
+/** Student */
+public class Student {
+
+    private Integer id;
+    private String name;
+
+    public Student() {}
+
+    public Student(Integer id) {
+        this.id = id;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Student other = (Student) obj;
+        return Objects.equals(id, other.id);
+    }
+}
