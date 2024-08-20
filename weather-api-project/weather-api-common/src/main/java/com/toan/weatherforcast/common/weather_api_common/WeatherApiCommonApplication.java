996938828abbe1570ba4ff33f66b11da1c81c8fc
07ductoan12 <07ductoan12@gmail.com>
2024-08-20 09:54:51 +0700

    add project weather APIs

---
+++ weather-api-project/weather-api-common/src/main/java/com/toan/weatherforcast/common/weather_api_common/WeatherApiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.toan.weatherforcast.common.weather_api_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiCommonApplication.class, args);
+	}
+
+}
