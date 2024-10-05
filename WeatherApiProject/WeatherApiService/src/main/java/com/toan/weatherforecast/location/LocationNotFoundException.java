28251e3ba4f7e9bcdca1747a8d1b1d84c9773454
07ductoan12 <07ductoan12@gmail.com>
2024-10-05 20:53:38 +0700

    rewrite project Weather Api

--- WeatherApiProject/WeatherApiService/src/main/java/com/toan/weatherforecast/location/LocationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.toan.weatherforecast.location;
-
-public class LocationNotFoundException extends Exception {
-    public LocationNotFoundException(String message) {
-        super(message);
-    }
-}
