1452d908c0b85e5afa3fdcd776ffdf82b3ef3116
07ductoan12 <07ductoan12@gmail.com>
2024-10-08 21:19:34 +0700

    add validate request body #1

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -2,13 +2,11 @@ package com.skyapi.weatherforecast.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 /** Location */
 @Entity
@@ -17,32 +15,36 @@ public class Location {
 
     @Column(length = 12, nullable = false, unique = false)
     @Id
-    @NotBlank
+    @NotBlank(message = "Location code cannot be left blank")
     private String code;
 
     @Column(length = 128, nullable = false)
     @JsonProperty("city_name")
-    @NotBlank
+    @NotBlank(message = "City name cannot be left blank")
     private String cityName;
 
     @Column(length = 128)
     @JsonProperty("region_name")
-    @NotNull
     private String regionName;
 
     @Column(length = 64, nullable = false)
     @JsonProperty("country_name")
-    @NotBlank
+    @NotBlank(message = "Country name cannot be left blank")
     private String countryName;
 
     @Column(length = 2, nullable = false)
     @JsonProperty("country_code")
-    @NotBlank
+    @NotBlank(message = "Country name cannot be left blank")
     private String countryCode;
+
+    public Location(@NotBlank(message = "Country name cannot be left blank") String countryCode) {
+        this.countryCode = countryCode;
+    }
 
     private boolean enabled;
 
-    @JsonIgnore private boolean trashed;
+    @JsonIgnore
+    private boolean trashed;
 
     public Location() {}
 
@@ -112,32 +114,25 @@ public class Location {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
         Location other = (Location) obj;
         if (code == null) {
-            if (other.code != null) return false;
-        } else if (!code.equals(other.code)) return false;
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Location [code="
-                + code
-                + ", cityName="
-                + cityName
-                + ", regionName="
-                + regionName
-                + ", countryName="
-                + countryName
-                + ", countryCode="
-                + countryCode
-                + ", enabled="
-                + enabled
-                + ", trashed="
-                + trashed
-                + "]";
+        return "Location [code=" + code + ", cityName=" + cityName + ", regionName=" + regionName
+                + ", countryName=" + countryName + ", countryCode=" + countryCode + ", enabled="
+                + enabled + ", trashed=" + trashed + "]";
     }
 }
